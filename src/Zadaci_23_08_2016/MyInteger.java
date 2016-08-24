e31d815bec91d455a3329c91e9c4c6ac8aac3f84
Eldin <Eldin@192.168.1.3>
2016-08-25 00:21:59 +0200

    Ispravak

--- src/Zadaci_23_08_2016/MyInteger.java
+++ src/Zadaci_23_08_2016/MyInteger.java
@@ -79,7 +79,7 @@ public static boolean isOdd(int num){
 
 	public static boolean isPrime(int num){
 		
-		for(int i = 0; i < num;i++){
+		for(int i = 2; i < num;i++){
 			if(num % i !=0)
 				return true;
 			
